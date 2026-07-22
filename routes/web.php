2a88b5107ee6ea12b34aec5c653bb11f2f5b3f79
ASUS <andergongut@hotmail.com>
2026-07-21 20:54:56 -0500

    Ruta temporal para migraciones

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,10 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/crear-admin', function () {
+Route::get('/migrar', function () {
+    Artisan::call('migrate', ['--force' => true]);
 
-    Role::firstOrCreate(['name' => 'admin']);
-
-    $user = User::updateOrCreate(
-        ['email' => 'admin@admin.com'],
-        [
-            'name' => 'Administrador',
-            'password' => Hash::make('Admin12345'),
-        ]
-    );
-
-    $user->assignRole('admin');
-
-    return 'Administrador creado correctamente';
+    return nl2br(Artisan::output());
 });
